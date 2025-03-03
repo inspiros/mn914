0e0bc7331753282b077fac364618fa4faf0a6e55
Hoang-Nhat Tran <hnhat.tran@gmail.com>
2025-03-03 17:18:14 +0100

    fix dcgan cifar10 architecture

--- stable_signature/models/dcgan/dcgan_cifar10.py
+++ stable_signature/models/dcgan/dcgan_cifar10.py
@@ -7,7 +7,7 @@ __all__ = ['Generator', 'Discriminator']
 class Generator(nn.Module):
     def __init__(self, nc: int = 3, nz: int = 100, ngf: int = 64):
         super(Generator, self).__init__()
-        self.main = self.main = nn.Sequential(
+        self.main = nn.Sequential(
             # input is Z, going into a convolution
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
@@ -21,13 +21,9 @@ class Generator(nn.Module):
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2, nc, 4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
+            # state size. (nc) x 32 x 32
         )
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
@@ -38,23 +34,19 @@ class Discriminator(nn.Module):
     def __init__(self, nc: int = 3, ndf: int = 64):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
+            # input is (nc) x 32 x 32
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
+            # state size. (ndf) x 16 x 16
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
+            # state size. (ndf*2) x 8 x 8
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            # state size. (ndf*4) x 4 x 4
+            nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
 
