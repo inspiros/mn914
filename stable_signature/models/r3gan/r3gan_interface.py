572526daf126a270293e4e2243b90fcbb5b40a67
Hoang-Nhat Tran <hnhat.tran@gmail.com>
2025-03-22 06:29:25 +0100

    add r3gan finetuning with critic loss

--- stable_signature/models/r3gan/r3gan_interface.py
+++ stable_signature/models/r3gan/r3gan_interface.py
@@ -1,12 +1,25 @@
+from typing import Tuple
+
 import torch
 import torch.nn as nn
 
-from .legacy import load_network_pkl
 from . import dnnlib
+from .legacy import load_network_pkl
 
-__all__ = ['get_generator']
+__all__ = ['get_generator', 'get_discriminator', 'get_both']
 
 
 def get_generator(f: str, dtype=torch.float, device=None) -> nn.Module:
     with dnnlib.util.open_url(f) as fp:
         return load_network_pkl(fp)['G_ema'].to(dtype=dtype, device=device)
+
+
+def get_discriminator(f: str, dtype=torch.float, device=None) -> nn.Module:
+    with dnnlib.util.open_url(f) as fp:
+        return load_network_pkl(fp)['D'].to(dtype=dtype, device=device)
+
+
+def get_both(f: str, dtype=torch.float, device=None) -> Tuple[nn.Module, nn.Module]:
+    with dnnlib.util.open_url(f) as fp:
+        nets = load_network_pkl(fp)
+        return nets['G'].to(dtype=dtype, device=device), nets['D'].to(dtype=dtype, device=device)
